d8b6bc7d215c3369e4c7d6818ee4af5b60e57910
thompson <thompsonmina7@gmail.com>
2020-09-16 17:14:35 +0100

    first commit of network project

---
+++ network/static/network/docs.js
@@ -0,0 +1,40 @@
+// credit goes to for the bs4 horizontal nav
+// https://github.com/mladenplavsic/bootstrap-navbar-sidebar/
+
+$(document).ready(function () {
+    $('[data-class]').click(function () {
+        updateNavbarClass($(this).attr('data-class'));
+    });
+
+    updateNavbarClass('fixed-left');
+});
+
+function updateNavbarClass(className) {
+    $('nav')
+        .removeClass(function (index, css) {
+            return (css.match(/(^|\s)fixed-\S+/g) || []).join(' ');
+        })
+        .addClass(className);
+
+    $('[data-class]').removeClass('active').parent('li').removeClass('active');
+    $('[data-class="' + className + '"]').addClass('active').parent('li').addClass('active');
+
+    fixBodyMargin(className);
+}
+
+function fixBodyMargin(className) {
+    if (/fixed-(left|right)/.test(className)) {
+        $('body').removeAttr('style');
+        if (className === 'fixed-right') {
+            $('body').css('marginLeft', 0);
+        } else {
+            $('body').css('marginRight', 0);
+        }
+    } else {
+        $('body').css({
+            "margin-right": 0,
+            "margin-left": 0,
+            "padding-top": '90px'
+        });
+    }
+}
